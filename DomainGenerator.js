b8cc374e23796b69df0515fc1cb6430127cf6fb7
Leticia Soto <samantasoto@gmail.com>
2024-09-21 03:12:53 +0000

    Se agrega .com al string de dominios generado

--- DomainGenerator.js
+++ DomainGenerator.js
@@ -5,7 +5,7 @@ let nouns = ["jogger", "racoon"];
 for ( let pronoun = 0; pronoun < pronouns.length; pronoun++ ){
   for( let adj = 0; adj < adjs.length; adj++ ){
     for (let noun = 0; noun < nouns.length; noun++) {
-      console.log(pronouns[pronoun]+adjs[adj]+nouns[noun]);
+      console.log(pronouns[pronoun]+adjs[adj]+nouns[noun]+".com");
       
     }
   }
