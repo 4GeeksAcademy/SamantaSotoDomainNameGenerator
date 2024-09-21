1e0b8c85cf0e5126937bd34c486bc26a50db516f
Leticia Soto <samantasoto@gmail.com>
2024-09-21 03:10:35 +0000

    Domain Generator
    recorre los arrays y concatena sus valores, falta el .com

---
+++ DomainGenerator.js
@@ -0,0 +1,12 @@
+let pronouns = ["the", "our"];
+let adjs = ["great", "big"];
+let nouns = ["jogger", "racoon"];
+
+for ( let pronoun = 0; pronoun < pronouns.length; pronoun++ ){
+  for( let adj = 0; adj < adjs.length; adj++ ){
+    for (let noun = 0; noun < nouns.length; noun++) {
+      console.log(pronouns[pronoun]+adjs[adj]+nouns[noun]);
+      
+    }
+  }
+}
